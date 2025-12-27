07fb30152def6cdfcd30a53c1b2f3ee0e8a23de9
Ymeh-Git <jeremy.droulez.pro@gmail.com>
2025-12-27 11:12:50 +0100

    Part 2 ex 8
    echo content

---
+++ Partie_2/SP_8/index.php
@@ -0,0 +1,44 @@
+<?php 
+// Connexion PHP-MySQL (MySQLi)
+
+// - [ X ]  **Exercice** : Afficher les livres de la bibliothèque sur une page web
+
+$server = "localhost"; 
+$user = "root"; // À changer si besoin, ici nous travaillons sur l'utilisateur basique
+$password = ""; // Par conséquent pas de MDP pour y accéder, en entreprise pour la sécurité un mot de passe sera précisé
+$dbName = "bibliothèque"; // Le nom de notre BDD à accéder
+
+// Pour récupérer les erreurs la connexion se fera dans un bloc try/catch 
+try {
+// Dans le bloc try on va bien sûr essayer de se connecter à la BDD
+$database = new PDO("mysql:host=$server;dbname=$dbName;charset=utf8mb4", $user, $password);
+
+// Notre demande en langage SQL vers la table "books" (considérons la table déjà remplie avec id, name, authors)
+$sql = "SELECT * FROM books";
+
+// Notre requête à partir de l'entrée en BDD et de notre requête (query = requête)
+$request = $database->query($sql);
+
+// Récupérons les résultats de notre requête et mettons les dans un tableau associatif (PDO::FETCH_ASSOC)
+$results = $request->fetchAll(PDO::FETCH_ASSOC);
+
+// Il faut maintenant l'afficher :
+// Ouverture de la liste non ordonnée (ici c'est mon choix)
+echo "<ul>";
+// Il faut boucler sur le résultats et que chaque entrées soient considérées comme $book
+// Bien entendu nous devons utiliser les noms précis de chaque champs dans notre BDD
+foreach($results as $book){
+    echo "<li> ~ ID : ".$book['id'];
+    echo "<ul>";
+    echo "<li> ~~ Nom : ".$book['name']."</li>";
+    echo "<li> ~~ Auteur : ".$book['authors']."</li>";
+    echo "</ul>";
+    echo "</li>";
+    echo "<br><br>";
+}
+// Ne pas oublier de fermer la balise <ul>
+echo "</ul>";
+} catch(PDOException $e){
+    // En cas d'erreur attrappée, l'afficher : 
+    echo "Une erreur est survenue : ".$e->getMessage();
+}
